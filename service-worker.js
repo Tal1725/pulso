f0d367dd08ea0b66a8bdf11067b1779790744913
Tal1725 <123480279+Tal1725@users.noreply.github.com>
2026-09-06 20:44:39 -0300

    feat: add PULSO service worker

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE = 'pulso-pwa-v1';
+const APP_SHELL = [
+  '/pulso/',
+  '/pulso/index.html',
+  '/pulso/manifest.webmanifest',
+  '/pulso/assets/css/styles.css',
+  '/pulso/assets/js/main.js',
+  '/pulso/assets/js/follow.js',
+  '/pulso/assets/js/seguidos.js',
+  '/pulso/assets/img/logo.svg',
+  '/pulso/assets/img/favicon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request).then(cached => cached || caches.match('/pulso/index.html')))
+  );
+});
